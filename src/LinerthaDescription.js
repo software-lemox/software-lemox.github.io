d9dcb25b09bc09cd5fb013588d6e914526b3b31c
kfirgo <kfirgo@rafael.co.il>
2025-09-01 13:58:37 +0300

    Set temporary main cover photo, start implement simple design of newsletter form

--- src/LinerthaDescription.js
+++ src/LinerthaDescription.js
@@ -20,8 +20,23 @@ function LinerthaDescription() {
                     <div className="App-description-section-content">
                         <a className="App-steam-icon"></a>
                     </div>
-                </div>                
+                </div>
+
             </section>
+
+            <section className="App-newsletter">
+                <h2 className="App-description-sub-title">Let's keep in touch</h2>
+                <h4>Join the newsletter to get monthly updates on my game development, new releases, and special offers.</h4>
+                <div className="App-newsletter-email">
+                    <input className="App-newsletter-email-input" type="email"></input>
+                    <input className="App-newsletter-email-button" type="button" value="Subscribe"></input>
+                </div>
+                <p className="App-newsletter-email-message">No spam. 1 email per month. Unsubscribe anytime.</p>
+            </section>
+
+                {/* TODO: Write 4 sections on the game */}
+                {/* <h2 className="App-description-sub-title">War tactics and great battles based on army blocks and shield walls</h2> */}
+            
         </>
     );
 }
